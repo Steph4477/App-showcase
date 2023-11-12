4d6166ee15b59c727f9798467db3aafb45b9ef34
Stéphane Morel <110528810+Steph4477@users.noreply.github.com>
2023-11-12 10:45:19 +0100

    update

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -9,7 +9,6 @@ import css from '../assets/css.png'
 import html from '../assets/html.svg'
 import git from '../assets/git.svg'
 
-
 export default function Description() {
   const words = [
     { technology: "React.js", image: react },
@@ -23,25 +22,22 @@ export default function Description() {
     { technology: "JavaScript", image: javascript },
   ];
   const [currentWordIndex, setCurrentWordIndex] = useState(0);
-
   useEffect(() => {
     const timer = setTimeout(() => {
       if (currentWordIndex < words.length - 1) {
         setCurrentWordIndex(currentWordIndex + 1);
       }
     }, 250);
-
     return () => clearTimeout(timer);
   }, [currentWordIndex]);
-
   return (
     <div>
       <div className="profil_container_description_p">
         <p>
           Merci de prendre le temps de consulter mon portfolio.
-          <br />
-          <br />
-          Actuellement en formation pour devenir Concepteur Développeur d'Applications Java chez Simplon, en CDD tremplin avec DSI entreprise adaptée.         
+          Actuellement en formation pour devenir : <br /><br />
+          <strong>Concepteur Développeur d'Applications Java</strong> <br /><br />
+          Chez Simplon, en CDD tremplin avec DSI entreprise adaptée.     
         </p>
       </div>
       <div className="skillsWord">
